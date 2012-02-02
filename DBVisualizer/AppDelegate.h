066be7013943a041899d709b764f33fa10ae23f1
Firas Kassem <firas@firas-kassems-macbook-pro.local>
2012-02-02 14:18:44 +0100

    Initial Commit

---
+++ DBVisualizer/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  DBVisualizer
+//
+//  Created by Firas Kassem on 2/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
